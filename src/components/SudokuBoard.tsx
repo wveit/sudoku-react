f99ed5f4d07060b3a11756fa93f1a3bde7d32ccd
wveit <wilbertveit@rocketmail.com>
2023-07-22 02:03:39 -0700

    Extract SudokuGrid from SudokuBoard
    to simplify the code inside SudokuBoard component

--- src/components/SudokuBoard.tsx
+++ src/components/SudokuBoard.tsx
@@ -1,3 +1,4 @@
+import { Children, ReactElement } from "react";
 import { cellsInEachSquare } from "../sudokulib/util";
 
 type Puzzle = number[];
@@ -7,13 +8,21 @@ interface SudokuBoardProps {
 }
 
 export function SudokuBoard({ puzzle }: SudokuBoardProps) {
+  const puzzleElements: ReactElement[] = puzzle.map((val, index) => (
+    <div key={index}>{val || " "}</div>
+  ));
+  return <SudokuGrid>{puzzleElements}</SudokuGrid>;
+}
+
+export function SudokuGrid({ children }: { children: ReactElement[] }) {
+  const childArray = Children.toArray(children);
   return (
     <div className="SudokuBoard">
       {cellsInEachSquare.map((square, squareIndex) => (
         <div key={squareIndex} className="SudokuBoard--square">
           {square.map((cellIndex, index) => (
             <div key={index} className="SudokuBoard--cell">
-              {puzzle[cellIndex] || " "}
+              {childArray[cellIndex]}
             </div>
           ))}
         </div>
